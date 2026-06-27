9beb18a1a6bcceddf50a1f8b5195d1916401ac1c
jusw444 <almazanjoshuanoel@gmail.com>
2026-06-27 18:15:38 +0800

    ts

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -4,7 +4,7 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // CSRF token for Laravel
-const token = document.head.querySelector('meta[name="csrf-token"]');
+const token = document.head.querySelector<HTMLMetaElement>('meta[name="csrf-token"]');
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 }
