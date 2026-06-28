d3802ea053ac201198cf505cc25f237b6e6f4a91
jusw444 <almazanjoshuanoel@gmail.com>
2026-06-28 16:11:03 +0800

    Fix contact email recipient to my Gmail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,7 +17,7 @@ class ContactController extends Controller
         ]);
 
         try {
-            Mail::to(config('mail.from.address'))->send(new ContactMessage(
+            Mail::to('almazanjoshuanoel@gmail.com')->send(new ContactMessage(
                 $validated['name'],
                 $validated['email'],
                 $validated['message']
@@ -27,7 +27,6 @@ class ContactController extends Controller
                 'success' => true,
                 'message' => 'Your message has been sent successfully!'
             ]);
-
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
